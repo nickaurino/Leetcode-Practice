5edd72e493f71db0b3d882868f2f5f163e15b37c
Nick Aurino <112521479+nickaurino@users.noreply.github.com>
2025-10-24 12:20:43 -0700

    Fix calculation of mid index in binary search

--- search.py
+++ search.py
@@ -3,7 +3,7 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left <= right:
-            mid = right + left // 2
+            mid = (right + left) // 2
             if (nums[mid] == target):
                 return mid
             elif (target > nums[mid]):
